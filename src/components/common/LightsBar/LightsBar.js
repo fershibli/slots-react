824fb46ab593223c923d439e952662010a9363e8
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-07-17 22:44:12 -0300

    fix(LightsBar): className and direction

--- src/components/common/LightsBar/LightsBar.js
+++ src/components/common/LightsBar/LightsBar.js
@@ -4,11 +4,11 @@ import "./LightsBar.css";
 
 const LightsBar = () => {
     return (
-        <InfiniteLooper speed={1} direction={"vertical"}>
-            <div class="led gold" />
-            <div class="led red" />
-            <div class="led green" />
-            <div class="led blue" />
+        <InfiniteLooper speed={1} direction={"up"}>
+            <div className="led gold" />
+            <div className="led red" />
+            <div className="led green" />
+            <div className="led blue" />
         </InfiniteLooper>
     );
 };
