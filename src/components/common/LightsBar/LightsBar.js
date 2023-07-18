381ed80efe18cfdd44048957029a1aed333963e2
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-07-17 22:52:31 -0300

    fix(LightsBar): prevent row direction

--- src/components/common/LightsBar/LightsBar.js
+++ src/components/common/LightsBar/LightsBar.js
@@ -5,10 +5,12 @@ import "./LightsBar.css";
 const LightsBar = () => {
     return (
         <InfiniteLooper speed={1} direction={"up"}>
-            <div className="led gold" />
-            <div className="led red" />
-            <div className="led green" />
-            <div className="led blue" />
+            <div className="led-bar">
+                <div className="led gold"></div>
+                <div className="led red"></div>
+                <div className="led green"></div>
+                <div className="led blue"></div>
+            </div>
         </InfiniteLooper>
     );
 };
